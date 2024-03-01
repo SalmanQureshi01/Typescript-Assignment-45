153b764e46342c77a12e9ef6e11191cbae0411f6
SalmanQureshi01 <160734520+SalmanQureshi01@users.noreply.github.com>
2024-03-01 08:25:40 -0800

    Add files via upload

---
+++ 3 Namecase.ts
@@ -0,0 +1,19 @@
+let personName : string = '';
+
+personName =prompt('what is your name?') || '';
+
+let lowercase : string = personName.toLowerCase();
+let uppercase : string = personName.toUpperCase();
+let titlecase : string = personName.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).
+toLowerCase()).join('')
+
+if (personName !== null && personName !== ''){
+    alert(`hello ${personName},here are your name in:
+    Lowercase:${lowercase}
+    Uppercase ${uppercase}
+    Titlecase ${titlecase}`)
+} 
+else{'please fill your name !'
+}
+
+
